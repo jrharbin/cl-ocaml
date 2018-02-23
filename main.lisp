dabf93434a088a607e59b0f3e87f63f70f120ca3
jrharbin <36747589+jrharbin@users.noreply.github.com>
2018-02-23 01:09:04 +0000

    Update main.lisp
    Delete redundant code shared with repl.lisp

--- main.lisp
+++ main.lisp
@@ -1,10 +1,1 @@
-(defun test-parse (str)
-  (debug-print-lexing str)
-  (yacc:parse-with-lexer (ocaml-lexer str)
-			 *ocaml-parser*))
-		  
-(defun test ()
-  (test-parse "x"))
 
-(defun repl ()
-  )
